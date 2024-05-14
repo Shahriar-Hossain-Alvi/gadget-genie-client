687721ed0e3ae26fd94b7353ecb30bf74d9f2089
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-05-14 19:13:24 +0600

    allservice page responsive done

--- src/components/Pages/Services/ServiceCard.jsx
+++ src/components/Pages/Services/ServiceCard.jsx
@@ -6,20 +6,20 @@ const ServiceCard = ({ service }) => {
     const { _id, imgURL, description, price, serviceName, serviceArea, providerName, providerImage } = service;
 
     return (
-        <div className='border p-4 rounded-2xl border-primaryColor'>
-            <div className='grid grid-cols-5 gap-3 mb-4'>
+        <div className='border p-4 rounded-2xl border-primaryColor mx-2 lg:mx-0'>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3 mb-4'>
 
-                <div className='col-span-2 flex flex-col justify-between space-y-3'>
+                <div className='lg:col-span-2 flex flex-col justify-between text-center lg:text-left space-y-3'>
                     <div></div>
 
                     <div className='space-y-4'>
-                        <h2 className='font-bold font-montserrat text-4xl'>{serviceName}</h2>
+                        <h2 className='font-bold font-montserrat text-2xl md:text-3xl lg:text-4xl'>{serviceName}</h2>
                         <p className='text-secondaryColor'>{description.slice(0, 100)}...</p>
                     </div>
 
-                    <div>
+                    <div className='flex flex-col items-center lg:flex-none md:flex-none lg:items-start md:items-start'>
                         <h3 className='text-lg font-semibold mb-2 text-secondaryColor flex items-center gap-1'>Service Provider: <span><FaHandPointDown /></span></h3>
-                        <div className='flex items-center gap-3'>
+                        <div className='flex flex-col lg:flex-row items-center gap-3'>
                             <div className='order-2'>
                                 <img className='w-12 h-12 rounded-full' src={providerImage} alt="" />
                             </div>
@@ -31,7 +31,7 @@ const ServiceCard = ({ service }) => {
                     </div>
                 </div>
 
-                <div className='border-x flex border-x-thirdColor flex-col justify-center items-center gap-7'>
+                <div className='border-y border-y-thirdColor md:border-x flex lg:border-x md:border-x-thirdColor lg:border-x-thirdColor flex-col justify-center items-center py-3 md:py-0 lg:py-0 gap-7 md:border-y-0 lg:border-y-0'>
                     <div className='text-center'>
                         <p className='font-medium'>Starts From:</p>
                         <span className='font-montserrat text-2xl italic text-thirdColor font-bold'>${price}</span>
@@ -49,7 +49,7 @@ const ServiceCard = ({ service }) => {
                 </div>
 
 
-                <div className='col-span-2'>
+                <div className='lg:col-span-2'>
                     <img className='w-full h-72 rounded-2xl' src={imgURL} alt="" />
                 </div>
             </div>
