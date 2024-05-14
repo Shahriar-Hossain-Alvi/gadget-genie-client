38a00ea4612b3f1ac7c0f20da5928c82d56a1c04
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-05-14 20:21:02 +0600

    manage service  page responsive done

--- src/components/Pages/ManageService/ManageServiceCard.jsx
+++ src/components/Pages/ManageService/ManageServiceCard.jsx
@@ -9,14 +9,14 @@ const ManageServiceCard = ({ mySingleService, handleDelete }) => {
     return (
         <div>
             <div className='border p-4 rounded-2xl border-primaryColor'>
-                <div className='grid grid-cols-5 gap-3 mb-4'>
+                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3 mb-4'>
 
                     {/* left part */}
-                    <div className='col-span-2 flex flex-col justify-between space-y-3'>
+                    <div className='lg:col-span-2 flex flex-col justify-between space-y-3'>
                         <div></div>
 
                         <div className='space-y-4'>
-                            <h2 className='font-bold font-montserrat text-4xl'>{serviceName}</h2>
+                            <h2 className='font-bold font-montserrat text-2xl md:text-3xl lg:text-4xl'>{serviceName}</h2>
                             <p className='text-secondaryColor'>{description.slice(0, 100)}...</p>
                         </div>
 
@@ -31,7 +31,7 @@ const ManageServiceCard = ({ mySingleService, handleDelete }) => {
                     </div>
 
                     {/* middle part */}
-                    <div className='border-x flex border-x-thirdColor flex-col justify-center items-center gap-7'>
+                    <div className='border-y border-y-thirdColor md:border-x md:border-y-0 lg:border-y-0 lg:border-x flex md:border-x-thirdColor lg:border-x-thirdColor flex-col justify-center items-center py-5 md:py-0 lg:py-0 gap-7'>
                         <div className='text-center'>
                             <p className='font-medium'>Starts From:</p>
                             <span className='font-montserrat text-2xl italic text-thirdColor font-bold'>${price}</span>
@@ -46,7 +46,7 @@ const ManageServiceCard = ({ mySingleService, handleDelete }) => {
                     </div>
 
                     {/* image */}
-                    <div className='col-span-2'>
+                    <div className='lg:col-span-2'>
                         <img className='w-full h-72 rounded-2xl' src={imgURL} alt="" />
                     </div>
                 </div>
