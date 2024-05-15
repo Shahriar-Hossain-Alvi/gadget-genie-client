68bf9b18183009e7e4b99be290e9301ce285f8b7
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-05-15 21:31:39 +0600

    fix some designes

--- src/components/Pages/ManageService/ManageService.jsx
+++ src/components/Pages/ManageService/ManageService.jsx
@@ -66,7 +66,12 @@ const ManageService = () => {
                 <title>Manage Service | Gadget Genie</title>
             </Helmet>
 
-            <h1 className="text-center text-3xl md:text-4xl lg:text-4xl font-bold font-montserrat underline mt-5 mb-10">My Added Services</h1>
+            <h1 className="text-center text-3xl md:text-4xl lg:text-4xl font-bold font-montserrat underline mt-5 mb-10">Manage Services</h1>
+
+            {
+                myAddedServices.length === 0 && 
+                <p className="text-center font-medium text-2xl mb-8"> You have not added any services</p>
+            }
 
             <div className="space-y-5 mb-10 mx-2 lg:mx-0">
                 {
