7ccfd7e4d0bbc0db91778fa7807e70e0f78a3686
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-06-27 18:40:23 +0600

    added search option in all services page

--- src/components/Pages/Services/Services.jsx
+++ src/components/Pages/Services/Services.jsx
@@ -4,25 +4,30 @@ import { Fade } from "react-awesome-reveal";
 import { useEffect, useState } from "react";
 
 const Services = () => {
-    const [spinner, setSpinner] = useState(false);
+    // const [spinner, setSpinner] = useState(false);
     const [services, setServices] = useState([]);
+    const [searchedText, setSearchedText] = useState('');
 
     useEffect(() => {
-        setSpinner(true);
-        fetch('https://gadget-genie-server.vercel.app/services')
+        fetch(`https://gadget-genie-server.vercel.app/services?search=${searchedText}`)
             .then(res => res.json())
             .then(data => {
                 setServices(data);
-                setSpinner(false);
             })
-    }, [])
+    }, [searchedText])
 
-    if (spinner) {
+    if (services.length === 0) {
         return <div className="flex justify-center mt-40">
             <span className="loading loading-bars loading-lg"></span>
         </div>
     }
 
+    const handleSearch = e => {
+        e.preventDefault();
+        const getSearchedText = e.target.search.value;
+        console.log(getSearchedText);
+        setSearchedText(getSearchedText)
+    }
 
     return (
         <div className="mt-8 mb-10">
@@ -33,6 +38,12 @@ const Services = () => {
                 <h2 className="text-center font-bold text-4xl font-montserrat underline mt-5">All Services</h2>
                 <p className="text-center text-lg font-medium mt-3 mb-10">Discover all the services we provide here</p>
 
+                <form onSubmit={handleSearch} className="flex items-center justify-center gap-3 mb-10">
+                    <h2>Search by name</h2>
+                    <input name="search" type="text" className="bg-transparent border rounded-xl border-thirdColor px-2 py-1" />
+
+                    <input type="submit" value="Search" className="btn bg-thirdColor text-white hover:bg-primaryColor" />
+                </form>
 
                 <div className="space-y-4">
                     <Fade>
